ad491e5be64b6d13b64e3af3d85d158848de9289
marc-antoine888 <69995480+marc-antoine888@users.noreply.github.com>
2022-02-24 09:48:24 -0500

    Add files via upload

---
+++ ProjetWiki.php
@@ -0,0 +1,17 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <h1 class="text-primary">Bienvenue sur le wiki du centre d'aide en informatique du cégep de Joliette</h1>
+            <h3>Tiens c'est bien vide ici...</h3>
+            <img src="">
+        </div>
+    </body>
+</html>
