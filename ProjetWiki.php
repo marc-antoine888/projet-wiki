36933e38bb454dd70425deb96b61f3c3cabed197
marc-antoine888 <69995480+marc-antoine888@users.noreply.github.com>
2022-03-06 16:15:41 -0500

    Add files via upload

---
+++ ProjetWiki.php
@@ -0,0 +1,87 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body class="bg-dark text-white">
+        <div class="row m-2 col-12 border border-secondary border-start-0 border-bottom-2 border-top-0 border-end-0" >
+
+            <div class="col-3">
+
+                <h2>
+                    <svg href="#" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                    </svg>
+                    Retour au berCAIl
+                </h2>
+            </div>
+
+            <div class="col-4">
+
+                <div class="input-group rounded">
+                    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+                    <span class="input-group-text border-0" id="search-addon">
+                        <i class="fas fa-search"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg>
+                    </span>
+                </div>
+            </div>
+
+            <div class="col-5">
+                <h2>Autres options...</h2>
+            </div>
+
+        </div>
+
+        <div class="row m-2">
+
+            <div class="col-2 h-100">
+
+                <h5>Articles populaire</h5>
+                <br>
+                <h6>Tutoriel de matante Germaine sur les clés USB</h6>
+                <hr class="bg-white">
+                <h6>McDo présente la meilleure diète de 2022</h6>
+                <hr class="bg-white">
+                <h6>Tutoriel - Comment couler sa philo ...</h6>
+                <hr class="bg-white">
+                <h6>Tutoriel - Comment Devenir un jeune homme branché</h6>
+                <hr class="bg-white">
+
+            </div>
+
+            <div class="col-9 border border-secondary border-start-2 border-bottom-0 border-top-0 border-end-0">
+
+                <h2>Ukraine</h2>
+                <hr class="bg-white">
+
+                <p class="">L'Ukrainee (/y.kʁɛn/ ; en ukrainien : Україна, Oukraïna /ukrɑˈjinɑ/)
+                    est un État d'Europe orientale, le deuxième d'Europe par sa
+                    superficie et le premier entièrement européen. Elle est bordée par
+                    la mer Noire et la mer d'Azov au sud, frontalière avec la Russie au
+                    nord-est et à l'est, avec la Biélorussie au nord, avec la Pologne, la
+                    Slovaquie et la Hongrie à l'ouest et avec la Roumanie et la Moldavie
+                    au sud-ouest. La capitale et la ville la plus peuplée du pays est Kiev.
+                    <br><br>
+
+                    Les origines de la culture ukrainienne remontent à l'État slave oriental médiéval
+                    de la Rus' de Kiev. Après la chute de cette dernière à l'issue des invasions mongoles
+                    du xiiie siècle, le territoire fait l’objet de partitions et se voit revendiqué par
+                    plusieurs puissances étrangères, dont la république des Deux Nations et l'Empire
+                    ottoman. L'Hetmanat cosaque indépendant existe entre le xviie siècle et le xviiie
+                    siècle, avant d’intégrer l'Empire russe.
+
+                </p>
+
+            </div>
+
+        </div>
+
+    </body>
+</html>
